48a2eb4bfb076cdb93aca62e7256cfbdd75898bb
Kevin Johansson <joke18ni@student.ju.se>
2021-04-16 15:02:57 +0200

    added LineSensor lib

---
+++ Arduino/lib/LineSensor/LineSensor.h
@@ -0,0 +1,16 @@
+#ifndef LINESENSOR_H
+#define LINESENSOR_H
+
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <MeAuriga.h>
+
+
+namespace LineSensor{
+    bool RightSensorBlack();
+}    
+  
+
+
+
+#endif /* LINESENSOR_H */

b5ccec8948b44d66cef127eb813cf4168f37d894
Gabriel Hildingsson Brickman <higa18bv@student.ju.se>
2021-04-23 13:32:29 +0200

    started the draw and path classes

--- Mobile_Application/ImsThanosApplication/app/src/main/java/com/example/imsthanosapplication/CanvasActivity.kt
+++ Mobile_Application/ImsThanosApplication/app/src/main/java/com/example/imsthanosapplication/CanvasActivity.kt
@@ -3,41 +3,51 @@ package com.example.imsthanosapplication
 import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.OvalShape
 import android.graphics.drawable.shapes.PathShape
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+
+class TestData {
+    companion object {
+        var testPath: MutableList<Point> = mutableListOf()
+    }
+}
 
 class CanvasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canvas)
 
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        PathObject.height = displayMetrics.heightPixels
+        PathObject.width = displayMetrics.widthPixels
+
         var imageV = findViewById<ImageView>(R.id.imageV)
+        val bitmap: Bitmap = Bitmap.createBitmap(PathObject.width, PathObject.height, Bitmap.Config.ARGB_8888)
 
-        val bitmap: Bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
+        PathObject.startPoint = Point(PathObject.width/2,PathObject.height/2)
+        PathObject.addPoint(PathObject.startPoint)
 
-        var shapeDrawable: ShapeDrawable
+        //TEST
+        TestData.testPath.add(Point(1080/2,1794/2))
+        TestData.testPath.add(Point(540,1700))
+        TestData.testPath.add(Point(10,300))
+        TestData.testPath.add(Point(100,175))
+        TestData.testPath.add(Point(260,100))
+        TestData.testPath.add(Point(540,300))
+        TestData.testPath.add(Point(1080-260,100))
+        TestData.testPath.add(Point(980,175))
+        TestData.testPath.add(Point(1070,300))
+        TestData.testPath.add(Point(540,1700))
+        Log.d("hejsan",PathObject.height.toString())
+        Log.d("hejsan",PathObject.width.toString())
 
-        var path = Path()
-        path.moveTo(0F,12F)
-        path.lineTo(100F,12F)
-        path.moveTo(100F,12F)
-        path.lineTo(100F,100F)
+        PathObject.drawPath(bitmap)
 
-        var pathShape = PathShape(path,500F,500F)
-        shapeDrawable = ShapeDrawable(pathShape)
-        shapeDrawable.setBounds(100,500,600,800)
-        shapeDrawable.paint.color = Color.parseColor("#009191")
-        shapeDrawable.paint.style = Paint.Style.FILL_AND_STROKE
-        shapeDrawable.paint.strokeWidth = 10F
-        shapeDrawable.draw(canvas)
-
-        // now bitmap holds the updated pixels
-
-        // set bitmap as background to ImageView
         imageV.background = BitmapDrawable(resources, bitmap)
 
     }

b5edeef6482dfb41070ce7da9cd0043360df968e
sama1795 <48963666+sama1795@users.noreply.github.com>
2021-05-06 13:05:07 +0200

    UltrasonicSensor function implemented

---
+++ Arduino/lib/MowerLib/MowerLib.cpp
@@ -0,0 +1,228 @@
+#include "MowerLib.h"
+#include <MeAuriga.h>
+
+/*
+------------------------------------------------------------------------------------------
+----------------------------------- Move related functions -------------------------------
+------------------------------------------------------------------------------------------
+*/
+
+/*
+  Global variables related to move
+*/
+
+static MeEncoderOnBoard Encoder_1(SLOT1);
+static MeEncoderOnBoard Encoder_2(SLOT2);
+
+//static MeBluetooth bluetooth(PORT_2);
+
+void moveSetup(int direction, int speed)
+{
+
+  speed = speed / 100.0 * 255;
+  int leftSpeed = 0;
+  int rightSpeed = 0;
+
+  if (direction == FORWARDS)
+  {
+    leftSpeed = -speed;
+    rightSpeed = speed;
+  }
+  else if (direction == BACKWARDS)
+  {
+    leftSpeed = speed;
+    rightSpeed = -speed;
+  }
+  else if (direction == RIGHT)
+  {
+    leftSpeed = -speed;
+    rightSpeed = -speed;
+  }
+  else if (direction == LEFT)
+  {
+    leftSpeed = speed;
+    rightSpeed = speed;
+  }
+  else if (direction == STOP)
+  {
+    leftSpeed = 0;
+    rightSpeed = 0;
+  }
+  Encoder_1.setTarPWM(leftSpeed);
+  Encoder_2.setTarPWM(rightSpeed);
+}
+
+void drive()
+{
+  Encoder_1.loop();
+  Encoder_2.loop();
+}
+
+/** 
+
+* Updates the position of motor 1, called when getting an interrupt that indecate the motor is running.
+  This function was provided by the manufacturer.
+*/
+
+void isr_process_encoder1(void)
+{
+  if (digitalRead(Encoder_1.getPortB()) == uint8_t(0))
+  {
+    Encoder_1.pulsePosMinus();
+  }
+  else
+  {
+    Encoder_1.pulsePosPlus();
+  }
+}
+
+/** 
+
+* Updates the position of motor 1, called when getting an interrupt that indecate the motor is running.
+  This function was provided by the manufacturer.
+*/
+
+void isr_process_encoder2(void)
+{
+  if (digitalRead(Encoder_2.getPortB()) == uint8_t(0))
+  {
+    Encoder_2.pulsePosMinus();
+  }
+  else
+  {
+    Encoder_2.pulsePosPlus();
+  }
+}
+
+void motorPositionInterrupt()
+{
+  attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
+  attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+}
+
+void DelayAndDO(float seconds, void (*func)(void))
+{
+  if (seconds < 0.0)
+  {
+    seconds = 0.0;
+  }
+  unsigned long endTime = millis() + seconds * 1000;
+  while (millis() < endTime)
+    func();
+}
+
+/*
+------------------------------------------------------------------------------------------
+------------------------------------------Line Sensor related functions-------------------
+------------------------------------------------------------------------------------------
+*/
+
+/*
+  Global variables related to Line Sensor.
+*/
+
+MeLineFollower linefollower_9(9);
+
+bool lineSensorBlack()
+{
+
+  int ifBlackLine = (linefollower_9.readSensors() & 3);
+  bool blackLine;
+
+  if (ifBlackLine == 0)
+    blackLine = true;
+
+  else
+    blackLine = false;
+
+  return blackLine;
+}
+
+/*
+------------------------------------------------------------------------------------------
+-------------------------------Me Ultrasonic Sensor related functions---------------------
+------------------------------------------------------------------------------------------
+*/
+
+/*
+  Global variables related to Me Ultrasonic Sensor.
+*/
+MeUltrasonicSensor ultrasonic_10(10);
+bool UltrasonicSensor(int distance)
+{
+
+
+  if (ultrasonic_10.distanceCm()<distance)
+    return true;
+
+  else
+    return false;
+
+}
+
+/*
+------------------------------------------------------------------------------------------
+--------------------------------------Bluetooth related -----------------------------
+------------------------------------------------------------------------------------------
+*/
+
+String bluetoothReceive(){
+
+}
+
+void bluetoothTransmitt(String data){
+  
+}
+
+
+/*
+------------------------------------------------------------------------------------------
+--------------------------------------Mower behavoir related -----------------------------
+------------------------------------------------------------------------------------------
+*/
+
+void drivingLoop(int *state)
+{
+  switch (*state)
+  {
+  case BOUNDARY_CHECK: //check for the line.
+    if (lineSensorBlack())
+      *state = TURN_FROM_BOUNDARY;
+    else
+      *state = DRIVE_FORWARD;
+    break;
+  case DRIVE_FORWARD: // drive forward.
+    moveSetup(FORWARDS, 50);
+    drive();
+    // delay?
+    *state = BOUNDARY_CHECK; // check again, or maybe set an interrupt for line sensor?
+    break;
+  case TURN_FROM_BOUNDARY: // turn from the line
+    // stop
+    moveSetup(STOP, 0);
+    DelayAndDO(0.5, drive);
+    //back
+    moveSetup(BACKWARDS, 50);
+    DelayAndDO(0.5, drive);
+    //stop
+    moveSetup(STOP, 0);
+    DelayAndDO(0.5, drive);
+    //right
+    moveSetup(RIGHT, 50);
+    DelayAndDO(2.5, drive);
+
+    *state = BOUNDARY_CHECK;
+    break;
+
+  default:
+    moveSetup(STOP, 0);
+    //_delay(0.5,drive);
+    // what to do in case program ended up here
+    break;
+  }
+}
+
+/*
+  Global variables related to 
+*/
+
---
+++ Arduino/lib/MowerLib/MowerLib.h
@@ -0,0 +1,154 @@
+#ifndef MowerLib_H
+#define MowerLib_H
+
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+
+
+struct MowerIndicators{
+    int mode; // auto or man
+    int speed; 
+    int direction;
+
+};
+
+/*
+    move related
+*/
+
+#define STOP        0
+#define FORWARDS    1
+#define BACKWARDS   2
+#define LEFT        3
+#define RIGHT       4
+
+/** 
+
+* Sets up the direction and the speed of the motors.  
+
+* @param   direction    the overall direction of the mower.
+
+* @param   speed        a procentage that indicate the motor speed.
+
+*/ 
+
+void moveSetup(int direction, int speed);
+
+/** 
+
+* Starts both motors by callig Encoder_1.loop().
+  Encoder_1.loop() :
+   deliverd by the manufacturer,
+   runs respective motor according setup no more than 1 time every 40 ms.
+
+*/ 
+
+void drive();
+
+/** 
+
+* Excutes isr_process_encoder1 and isr_process_encoder2 when the an interrupt from respective motor occure.
+  This make the continues update of motors position possible.
+
+*/ 
+
+void motorPositionInterrupt();
+
+/** 
+
+* Excuting a function for certin period of time what allows to delay without blocking the program. 
+
+* 
+
+* @param   seconds the delay time.
+
+* @param   func    the function to be excuted.
+
+*/ 
+
+void DelayAndDO(float seconds,void (*func)(void));
+
+
+
+
+/*
+    line_sensor related
+*/
+
+/** 
+
+* Detects black colored lines.
+
+* @returns      blackLine which is true if the line is black and false otherwise.
+
+*/ 
+
+/*
+    Control the Me Ultrasonic Sensor.
+*/
+
+
+bool lineSensorBlack();
+
+/** 
+
+* Reads data from  Me Ultrasonic Sensor.
+*
+* @param    *distance value as a inte
+
+* @returns   bool.
+
+*/ 
+
+bool UltrasonicSensor(int distance);
+
+/*
+    Bluetooth related.
+*/
+
+/** 
+
+* Reads received data from bluetooth via Serial.
+ 
+
+* @returns   received data as a String.
+
+*/ 
+
+String bluetoothReceive();
+
+/** 
+
+* sends data to bluetooth via Serial.
+
+* @param   data data as a String to be sent.
+
+*/ 
+
+void bluetoothTransmitt(String data);
+
+/*
+    Mower behavior related.
+*/
+
+#define BOUNDARY_CHECK      0
+#define DRIVE_FORWARD       1
+#define TURN_FROM_BOUNDARY  2
+
+/** 
+
+* Steers the beahvior of the mower.
+* 
+
+* @param   *state the start state fo the machine
+
+*/
+
+void drivingLoop(int *state);
+
+
+
+#endif // header_H

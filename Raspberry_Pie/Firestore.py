0f31fea2979f763789ab94af936b707b8677f10a
Thanos RPI <joke18ni@student.ju.se>
2021-05-13 16:07:06 +0200

    Added working script for pyrebase connection to firebase

--- Raspberry_Pie/Firestore.py
+++ Raspberry_Pie/Firestore.py
@@ -1,21 +1,28 @@
-import firebase_admin
-from firebase_admin import credentials,firestore
-from datetime import datetime, timezone
+ #This code should be used with every script that you will be using to connect to the Firebase database.
+import pyrebase
+import time
 
-cred = credentials.Certificate("./ims-thanos-firebase-adminsdk-rxc8t-a522b3e4ae.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
 
-doc_ref = db.collection(u'users').document()
+config = {
+  # You can get all these info from the firebase website. It's associated with your account.
+  "apiKey": "AAAActQSqd0:APA91bHYXP7Y1PJGv0moMGKLQJQI87fHufCP5mv8tx3pNZxRQgJxRPkFnwwqvRTCBpMsRKIy42Ci3aq3C5d9yM2JxzaO-uEsTlK-_tZHLhXCCrhYlWQ2VqzWQj58oKQA24sk86pUNEUj",
+  "authDomain": "ims-thanos.firebaseapp.com",
+  "databaseURL": "https://ims-thanos-default-rtdb.europe-west1.firebasedatabase.app/",
+  "storageBucket": "ims-thanos.appspot.com"
+}
 
-while True:
-    #Get position from arduino and send to firebase with some interval
-    # x = 1
-    # y = 2
-    # now = datetime.now()
-    doc_ref.set({
-        u'x' : x,
-        u'y' : y,
-        u'time' : now
-    })
-    break
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+
+data = {
+  "list": [
+    {"x": 15,
+     "y": 15,},
+     {"x":20,
+     "y":20,}
+     ]
+}
+
+
+db.child("Routes").child(time.asctime(time.localtime(time.time()))).child("coordinates").push(data)

076ef89a5f045c73134b4fd941707114c3878c7b
emilchrs <emilkabusa@hotmail.com>
2021-05-20 13:50:23 +0200

    can read data from realtime

--- Mobile_Application/ImsThanosApplication/app/src/main/java/com/example/imsthanosapplication/CanvasActivity.kt
+++ Mobile_Application/ImsThanosApplication/app/src/main/java/com/example/imsthanosapplication/CanvasActivity.kt
@@ -2,6 +2,7 @@ package com.example.imsthanosapplication
 
 import android.graphics.Bitmap
 import android.graphics.Point
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
@@ -22,110 +23,51 @@ class TestData {
 
 class CanvasActivity() : AppCompatActivity() {
     val TAG = "DocSnippets"
-//    private lateinit var database : DatabaseReference
-   // private lateinit var database: DatabaseReference
-    private val database = Firebase.database
-    private val myRef = database.getReference("mowerPositions")
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canvas)
-        //  val db = FirebaseFirestore.getInstance()
 
-     //   myRef.setValue("Hello, World!")
-
-
-    //    readDataFromRealtimeDatabase()
-     //   myRef.setValue("Hello, World!")
         val displayMetrics = DisplayMetrics()
-      //  readData()
+
         windowManager.defaultDisplay.getMetrics(displayMetrics)
         PathObject.height = displayMetrics.heightPixels
         PathObject.width = displayMetrics.widthPixels
-        //myRef.get()
         var imageV = findViewById<ImageView>(R.id.imageV)
         val bitmap: Bitmap =
             Bitmap.createBitmap(PathObject.width, PathObject.height, Bitmap.Config.ARGB_8888)
-/*
-        val docRef = db.collection("Path")
-        docRef.get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    PathObject.startPoint = Point(PathObject.width/2,PathObject.height/2)
-                    PathObject.addPoint(PathObject.startPoint)
-                    val x = document.get("x")
-                    val y = document.get("y")
-                    TestData.testPath.add(Point(x.hashCode(), y.hashCode()))
-                    PathObject.drawPath(bitmap)
-                    imageV.background = BitmapDrawable(resources, bitmap)
-                    Log.d(TAG, "${document.id} => ${document.data}")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents: ", exception)
-            }*/
 
 
-/*
-        myRef.addValueEventListener(object : ValueEventListener {
-
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                Log.d(TAG, database.toString())
-                Log.d(TAG, myRef.toString())
-                Log.d(TAG, "Here")
-                //myRef.setValue("Hello, World!")
-                val value = dataSnapshot.getValue<String>()
-                //val value = dataSnapshot.getValue(MowerPosition::class.java)!!
-                Log.d(TAG, "Value is: $value")
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })*/
-        readDataFromRealtimeDatabase ()
-}
-
-
-   private fun readDataFromRealtimeDatabase () {
-
-       var database = FirebaseDatabase.getInstance().getReference("Routes")
+        var database = FirebaseDatabase.getInstance().reference
 
         Log.d(TAG, database.toString())
         Log.d(TAG, database.get().toString())
 
-
-        database.setValue("Hello, World!")
+        
 
         database.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
                 Log.d(TAG,"Exists1")
-                val value = snapshot.getValue<String>()
 
-                Log.d(TAG, "Value is: $value")
-                val mPos = snapshot.getValue<Response>()
-                Log.d(TAG, "Value is: $mPos")
+
+
                 if (snapshot.exists()) {
                     Log.d(TAG,"Exists")
                     val data = snapshot.child("Routes").children
 
-                    Log.d(TAG, "hilloooo")
                     data.forEach {
+                        Log.d(TAG, "hilloooo")
                         Log.d(TAG, it.toString())
-                   //     val traveledPathSession = Response(mutableListOf())
-                   //     val traveledPaths = it.children
-                   //     traveledPaths.forEach{ pathData ->
-                    //        val traveledPath = pathData.getValue(mowerPosition::class.java)
-                        //    traveledPathSession.traveledPaths.add(traveledPath!!)
-                        }
-                       // Globals.traveledPathSessionList.add(traveledPathSession)
-                  //  }
-                   // traveledPathSessions.value = Globals.traveledPathSessionList
+
+                        PathObject.startPoint = Point(PathObject.width/2,PathObject.height/2)
+                        PathObject.addPoint(PathObject.startPoint)
+                        val mowPos = it.getValue(MowerPosition::class.java)
+                        val x = mowPos!!.x
+                        val y = mowPos!!.y
+                        TestData.testPath.add(Point(x.hashCode(), y.hashCode()))
+                        PathObject.drawPath(bitmap)
+                        imageV.background = BitmapDrawable(resources, bitmap)
+                    }
                 }
                 else{
                     Log.d(TAG, "snapshot does not exist")
@@ -133,10 +75,13 @@ class CanvasActivity() : AppCompatActivity() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-               Log.d(TAG,"Canceld")
+                Log.d(TAG,"Canceld")
             }
         })
-    }
+
+
+}
+
 
 
     data class MowerPosition(
